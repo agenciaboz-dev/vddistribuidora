375b8cae15fce76925accad1c89788513ed5bfbf
alfredo-petri <alfredo_petri@hotmail.com>
2024-03-28 17:35:05 -0300

    Revert "update AddButton"
    This reverts commit 3884c193f7fcaa8017421882f1d5175d5489af38.

--- src/components/AddButton.tsx
+++ src/components/AddButton.tsx
@@ -1,27 +1,21 @@
 import { Box, Button } from '@mui/material'
 import AddIcon from '@mui/icons-material/Add';
 
-interface IButtonAdd {
-    name: string
-    onClick: () => void
-}
-
-export const AddButton: React.FC<IButtonAdd> = ({ onClick, name }) => {
-    return (
-        <Box>
-            <Button
-                variant="contained"
-                startIcon={<AddIcon />}
-                sx={{
-                    borderRadius: "2vw",
-                    whiteSpace: "nowrap",
-                    p: "0 1vw",
-                    color: "primary.white",
-                }}
-                onClick={onClick}
-            >
-                Adicionar {name}
-            </Button>
-        </Box>
-    )
+export const AddButton = () => {
+  return (
+    <Box>
+        <Button
+            variant="contained"
+            startIcon={<AddIcon />}
+            sx={{
+              borderRadius: "2vw",
+              whiteSpace: "nowrap",
+              p: "0 1vw",
+              color:"primary.white"
+            }}
+          >
+            Adicionar Pedido
+          </Button>
+    </Box>
+  )
 }
--- src/pages/Main/subPages/Activities/localComponents/PedidosEntrada.tsx
+++ src/pages/Main/subPages/Activities/localComponents/PedidosEntrada.tsx
@@ -1,43 +1,32 @@
-import React, { useState } from "react"
-import { Box, Button } from "@mui/material"
-import { SearchField } from "./ActivitiesSideMenu/SearchField"
-import { List } from "../../../../../components/List"
-import { AddButton } from "../../../../../components/AddButton"
-import { TotalParcial } from "./ActivitiesSideMenu/TotalParcial"
-import { useNavigate } from "react-router-dom"
-import { ModalAdicionarProdutos } from "./ActivitiesSideMenu/ModalAdicionarProduto"
+import React from "react";
+import { Box  } from "@mui/material";
+import { SearchField } from "./ActivitiesSideMenu/SearchField";
+import { List } from "../../../../../components/List";
+import { AddButton } from "../../../../../components/AddButton";
+
+
 
 interface pedidoEntrada {}
 
 export const PedidosEntrada: React.FC<pedidoEntrada> = ({}) => {
-    const [open, setOpen] = useState(false)
-
-    const handleClickOpen = () => {
-        setOpen(true)
-    }
-    
-    const handleClose = () => {
-        setOpen(false)
-    }
-
-    return (
-        <Box sx={{ flexDirection: "column", p: "2vw", pr: "4vw", width: "100%", gap: "1vw" }}>
-            <Box sx={{ flexDirection: "column", backgroundColor: "#fff" }}>
-                <Box sx={{ fontSize: "2rem" }}>Pedido de Entrada</Box>
-                <Box sx={{ color: "secondary.main" }}>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure atque consectetur minima maxime ipsum enim aut nobis alias
-                    reprehenderit, est illum quos? Minima aperiam reiciendis magnam ut ex maxime laudantium?
-                </Box>
-            </Box>
-            <Box sx={{ Color: "secondary", mt: "1.5vw", gap: "1vw", width: "100%" }}>
-                <SearchField />
-                <AddButton onClick={handleClickOpen} name="pedido" />
-            </Box>
-            <Box sx={{ width: "95%" }}>
-                <List />
-            </Box>
-            <TotalParcial />
-            <ModalAdicionarProdutos close={handleClose} open={open} />
+  return (
+    <Box sx={{ flexDirection: "column", p: "2vw", pr: "4vw", width: "100%", gap:"1vw"}}>
+      <Box sx={{ flexDirection: "column", backgroundColor: "#fff" }}>
+        <Box sx={{ fontSize: "2rem" }}>Pedido de Entrada</Box>
+        <Box sx={{ color: "secondary.main" }}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure atque
+          consectetur minima maxime ipsum enim aut nobis alias reprehenderit,
+          est illum quos? Minima aperiam reiciendis magnam ut ex maxime
+          laudantium?
         </Box>
-    )
-}
+      </Box>
+      <Box sx={{ Color: "secondary", mt: "1.5vw", gap: "1vw", width: "100%" }}>
+        <SearchField />
+        <AddButton />
+      </Box>
+      <Box sx={{width:"95%"}}>
+        <List />
+      </Box>
+    </Box>
+  );
+};
